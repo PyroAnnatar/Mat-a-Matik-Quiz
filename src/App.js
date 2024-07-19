ac8cdc9a04a117f4c2dfd94c6dacf18abd981e76
PyroAnnatar <pyroannatar@gmail.com>
2024-07-19 20:34:17 +0300

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,129 +1,129 @@
-import './styles.css'
-import { useEffect, useState } from 'react'
-import CheckAnswerButton from './components/CheckAnswerButton'
-import GetNewProblemButton from './components/GetNewProblemButton'
-import StartButton from './components/StartButton'
+import "./styles.css";
+import { useEffect, useState } from "react";
+import CheckAnswerButton from "./components/CheckAnswerButton";
+import GetNewProblemButton from "./components/GetNewProblemButton";
+import StartButton from "./components/StartButton";
 
 export default function App() {
-  const [mathProblem, setMathProblem] = useState(getRandomProblem)
-  const [currentResponse, setCurrentResponse] = useState('')
-  const [recentCorrectAnswer, setRecentCorrectAnswer] = useState(false)
-  const [recentStatusChange, setRecentStatusChange] = useState(false)
-  const [answerStatus, setAnswerStatus] = useState('')
-  const [gameStarted, setGameStarted] = useState(false)
-  const [isHydrated, setIsHydrated] = useState(false)
+  const [mathProblem, setMathProblem] = useState(getRandomProblem);
+  const [currentResponse, setCurrentResponse] = useState("");
+  const [recentCorrectAnswer, setRecentCorrectAnswer] = useState(false);
+  const [recentStatusChange, setRecentStatusChange] = useState(false);
+  const [answerStatus, setAnswerStatus] = useState("");
+  const [gameStarted, setGameStarted] = useState(false);
+  const [isHydrated, setIsHydrated] = useState(false);
 
   useEffect(() => {
     // Sayfa client tarafında çalıştığında
-    setIsHydrated(true)
-  }, [])
+    setIsHydrated(true);
+  }, []);
 
   function getRandomProblem() {
-    const firstNum = getRandomNumber(10)
-    let secondNum = getRandomNumber(10)
-    const operator = getRandomOperator()
-    if (operator === '÷' && secondNum === 0) {
-      secondNum = 1 + getRandomNumber(9)
+    const firstNum = getRandomNumber(10);
+    let secondNum = getRandomNumber(10);
+    const operator = getRandomOperator();
+    if (operator === "÷" && secondNum === 0) {
+      secondNum = 1 + getRandomNumber(9);
     }
     const mathProblem = {
       string: `${firstNum} ${operator} ${secondNum} =`,
       answer: getCorrectAnswer(firstNum, secondNum, operator).toFixed(2),
       submittedResponse: undefined,
-    }
-    return mathProblem
+    };
+    return mathProblem;
   }
 
   function getNewProblem() {
-    setAnswerStatus('')
-    setCurrentResponse('')
-    setMathProblem(getRandomProblem)
+    setAnswerStatus("");
+    setCurrentResponse("");
+    setMathProblem(getRandomProblem);
   }
 
   function getCorrectAnswer(firstNum, secondNum, operator) {
-    const x = firstNum
-    const y = secondNum
-    if (operator === '+') {
-      return x + y
-    } else if (operator === '-') {
-      return x - y
-    } else if (operator === 'x') {
-      return x * y
+    const x = firstNum;
+    const y = secondNum;
+    if (operator === "+") {
+      return x + y;
+    } else if (operator === "-") {
+      return x - y;
+    } else if (operator === "x") {
+      return x * y;
     } else {
-      return x / y
+      return x / y;
     }
   }
 
   function getAnswerStatus(num) {
     if (num < mathProblem.answer) {
-      return 'Düşük'
+      return "Düşük";
     } else if (num > mathProblem.answer) {
-      return 'Yüksek'
+      return "Yüksek";
     } else if (num == mathProblem.answer) {
-      return 'Doğru!'
+      return "Doğru!";
     } else {
-      return 'Yanlış Girdi.'
+      return "Yanlış Girdi.";
     }
   }
 
   function getRandomNumber(maximum) {
-    return Math.round(Math.random() * maximum)
+    return Math.round(Math.random() * maximum);
   }
 
   function getRandomOperator() {
-    const operators = ['+', '-', 'x', '÷']
-    return operators[getRandomNumber(3)]
+    const operators = ["+", "-", "x", "÷"];
+    return operators[getRandomNumber(3)];
   }
 
   function updateResponse(e) {
-    setCurrentResponse(e.target.value)
+    setCurrentResponse(e.target.value);
   }
 
   function handleSubmit(e) {
-    e.preventDefault()
-    let userAnswer = +(+currentResponse).toFixed(2)
+    e.preventDefault();
+    let userAnswer = +(+currentResponse).toFixed(2);
     setMathProblem((prevData) => ({
       ...prevData,
       submittedResponse: userAnswer,
-    }))
-    setAnswerStatus(() => getAnswerStatus(userAnswer))
-    setRecentStatusChange(true)
+    }));
+    setAnswerStatus(() => getAnswerStatus(userAnswer));
+    setRecentStatusChange(true);
     if (mathProblem.answer == userAnswer) {
-      setRecentCorrectAnswer(true)
+      setRecentCorrectAnswer(true);
     }
   }
 
   useEffect(() => {
-    let timeOut
+    let timeOut;
     if (recentCorrectAnswer) {
       timeOut = setTimeout(() => {
-        setRecentCorrectAnswer(false)
-        getNewProblem()
-      }, 2000)
+        setRecentCorrectAnswer(false);
+        getNewProblem();
+      }, 2000);
     }
-    return () => clearTimeout(timeOut)
-  }, [recentCorrectAnswer])
+    return () => clearTimeout(timeOut);
+  }, [recentCorrectAnswer]);
 
   useEffect(() => {
-    let timeOut
+    let timeOut;
     if (recentStatusChange) {
       timeOut = setTimeout(() => {
-        setRecentStatusChange(false)
-      }, 2000)
+        setRecentStatusChange(false);
+      }, 2000);
     }
-    return () => clearTimeout(timeOut)
-  }, [recentStatusChange])
+    return () => clearTimeout(timeOut);
+  }, [recentStatusChange]);
 
-  let inputClass = gameStarted ? '' : 'hidden '
+  let inputClass = gameStarted ? "" : "hidden ";
 
-  if (answerStatus === 'Doğru!') {
-    inputClass += 'input-accepted'
+  if (answerStatus === "Doğru!") {
+    inputClass += "input-accepted";
   }
 
   const messageClass = answerStatus
     .toLowerCase()
-    .split(' ')
-    .join('-')
-    .slice(0, -1)
+    .split(" ")
+    .join("-")
+    .slice(0, -1);
 
   const gamePlayButtons = [
     <GetNewProblemButton
@@ -135,8 +135,8 @@ export default function App() {
       disabled={recentCorrectAnswer}
       key={crypto.randomUUID()}
     />,
-  ]
-  const startButton = <StartButton clickHandler={() => setGameStarted(true)} />
+  ];
+  const startButton = <StartButton clickHandler={() => setGameStarted(true)} />;
   function showStates() {
     const states = {
       mathProblem,
@@ -145,42 +145,42 @@ export default function App() {
       recentStatusChange,
       answerStatus,
       gameStarted,
-    }
+    };
 
-    const line = '-----------------------------'
-    const space = '‎ '
-    const timeStamp = new Date().toLocaleTimeString()
+    const line = "-----------------------------";
+    const space = "‎ ";
+    const timeStamp = new Date().toLocaleTimeString();
 
     for (const state in states) {
       if (states[state] === mathProblem) {
-        console.log(space)
-        console.log(line)
-        console.log(`Durumlar ${timeStamp} itibarıyla`)
-        console.log(space)
+        console.log(space);
+        console.log(line);
+        console.log(`Durumlar ${timeStamp} itibarıyla`);
+        console.log(space);
       }
 
       const value =
-        states[state] === ''
+        states[state] === ""
           ? `""`
-          : typeof states[state] === 'object'
+          : typeof states[state] === "object"
           ? stringify(states[state])
-          : states[state]
+          : states[state];
 
       function stringify(obj) {
         const string = Object.entries(obj)
           .map(([key, value]) => {
             const parsedValue =
-              typeof value === 'undefined' ? value : `"${value}"`
-            return `${key}: ${parsedValue}`
+              typeof value === "undefined" ? value : `"${value}"`;
+            return `${key}: ${parsedValue}`;
           })
-          .join(', ')
-        return `{${string}}`
+          .join(", ");
+        return `{${string}}`;
       }
 
-      console.log(state + ' = ' + value)
+      console.log(state + " = " + value);
 
-      if (state !== 'gameStarted') {
-        console.log(space)
+      if (state !== "gameStarted") {
+        console.log(space);
       }
     }
   }
@@ -200,36 +200,37 @@ export default function App() {
                
         Not: Görevi tamamlamak için *sadece* aşağıdaki return deyiminin içine küçük bir kod yazmanız gerekmektedir. Bu veya başka bir dosyada yazmanız gereken başka bir kod yok
 */
-
+  showStates();
   return (
-    <div className='wrapper'>
-      <h1>Mat-a-Matik</h1>
+    <div className="wrapper">
+      {!gameStarted && <h1>Mat-a-Matik</h1>}
       {isHydrated && (
         <form onSubmit={handleSubmit}>
           <label>
-            <div className='problem-container'>{mathProblem.string}</div>
+            {gameStarted && (
+              <div className="problem-container">{mathProblem.string}</div>
+            )}
             <input
-              type='number'
-              name='value'
-              placeholder='?'
+              type="number"
+              name="value"
+              placeholder="?"
               onChange={updateResponse}
               value={currentResponse}
               className={inputClass}
-              autoComplete='off'
+              autoComplete="off"
               required
             />
           </label>
 
           <div className={`message-container ${messageClass}`}>
-            {answerStatus}
+            {recentStatusChange && answerStatus}
           </div>
 
-          <div className='button-container'>
-            {gamePlayButtons}
-            {startButton}
+          <div className="button-container">
+            {gameStarted ? gamePlayButtons : startButton}
           </div>
         </form>
       )}
     </div>
-  )
+  );
 }
